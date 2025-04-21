338d9336caaf0ad1cdab912f3e58aede798d1a4f
WilliamsAl282 <WilliamsAl282@northwested.org>
2025-04-21 10:45:09 -0400

    work

--- game.py
+++ game.py
@@ -4,24 +4,34 @@ import config # Import the config Module
 import random
 import shapes
 
+pygame.init()
+pygame.font.init()
+pygame.mixer.init()
+
 
 def init_game():
-    pygame.init()
-    pygame.font.init()
-    pygame.mixer.init()
+    # pygame.init()
+    # pygame.font.init()
+    # pygame.mixer.init()
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT)) # Use constanst from config
     pygame.display.set_caption(config.TITLE)
     return screen
-
+character_sound = pygame.mixer.Sound(config.SOUND_EFFECT_CHARACTER)
+bonus_sound = pygame.mixer.Sound(config.SOUND_EFFECT_BONUS)
 def handle_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
-        elif event.type == pygame.KEYDOWN:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            character_sound.play()
+        if keys[pygame.K_b]:
+            bonus_sound.play()
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 return False
-        if keys[pygame.K_p]:
-            
+        
+
     return True
 
 
@@ -33,22 +43,22 @@ def draw_rectangle(screen, color, x, y, width, height):
     pygame.draw.rect(screen,color,x,y,width,height)
 
 def main():
+
+
     screen = init_game()
     running = True
     clock = pygame.time.Clock() # Initialize the clock her
     
     text_font = pygame.font.SysFont('Arial', 30, bold = False, italic = False)
 
-    base_y = 30
-
-    line_height = 20
+    
 
     while running:
         running = handle_events()
         screen.fill(config.BLUE) # Use color from config
         
         draw_text(screen, "Press 'a' to play the bonus sound effect", text_font, config.WHITE, 90,300 )
-        draw_text(screen, "Press 'b' to play the character sound effect", text_font, config.WHITE, 130,300 )
+        draw_text(screen, "Press 'b' to play the character sound effect", text_font, config.WHITE, 90,450 )
         pygame.display.flip()
 
         # Limit the frame rate to the specified frames per second (FPS)
